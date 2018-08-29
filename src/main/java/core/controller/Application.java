337fa1de95d16c934b217e59e64701c59d62d51d
oufeilong <oufeilong_cn@yeah.net>
2018-08-29 16:27:37 +0800

    changes for package as war file

--- src/main/java/core/controller/Application.java
+++ src/main/java/core/controller/Application.java
@@ -2,16 +2,23 @@ package core.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Application {
+public class Application extends SpringBootServletInitializer{
 	
 	@RequestMapping("/")
     public String greeting() {
         return "Hello World!";
+    }
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 	
 	public static void main(String[] args) {
